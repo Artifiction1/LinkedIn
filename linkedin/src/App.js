3ec9fc2a13254b6036e5a8c938bca1750db59bc7
marios <mgeorgak859@gmail.com>
2022-07-11 22:13:19 +0300

    added hardcoded newPost component

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import GetPosts from './components/GetPosts';
+import NewPost from './components/NewPost';
 
 function App() {
   return (
     <div className="App">
       <GetPosts />
+      <NewPost />
     </div>
   );
 }
--- linkedin/src/components/NewPost.jsx
+++ linkedin/src/components/NewPost.jsx
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
-import { newPostAction } from '../redux/actions'
 
-const mapStateToProps = (state) => {
-    return{
-   
-    }
-}
-
-const mapDispatchToProps =(dispatch) => {
-    return {
-        newPostList: () => {
-            dispatch(newPostAction())
-        }
-    }
-}
 
 
 function NewPost () {
     useEffect(() => {
-        newPostList();
+        newPost();
       }, []);
+
+const newPost = async () => {
+    try {
+        let text = { text: "hey new post" };
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/posts/",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-type": "application/json",
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmNiZWNlY2U2YzAzMDAwMTU5MTgxNDMiLCJpYXQiOjE2NTc1MzE2MjgsImV4cCI6MTY1ODc0MTIyOH0.Ueo_M62QO05ffN1aYIPJjOyI14bH3uldPPo-OlagobM",
+            },
+            body: JSON.stringify(text),
+          }
+        );
+        if (response.ok) {
+          let newPost = await response.json();
+          console.log(newPost);
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
     return(
         <div>NewPost</div>
     )
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewPost)
+export default NewPost
