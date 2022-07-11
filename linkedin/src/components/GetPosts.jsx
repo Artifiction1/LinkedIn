9c98f91ebee8b860ec62db6130456c81635795ad
marios <mgeorgak859@gmail.com>
2022-07-11 15:51:21 +0300

    added store and getPosts component/action

--- linkedin/src/components/GetPosts.jsx
+++ linkedin/src/components/GetPosts.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { getPostsAction } from '../redux/actions'
 
-export default function GetPosts() {
-  return (
-    <div>GetPosts</div>
-  )
+const mapStateToProps = (state) => {
+    return{
+
+    }
 }
+const mapDispatchToProps =(dispatch) => {
+    return {
+        getPostsList: ()=>{
+            dispatch(getPostsAction())
+        }
+    }
+}
+
+function GetPosts() {
+    return (
+        <div>GetPosts</div>
+    )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GetPosts)
---
+++ linkedin/src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    posts: {
+        postsList:[]
+    }
+}
+
+const mainReducer = (state= initialState, action) =>{
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
