20569d16fa04c41296a1c1f24d8ce3a9511bfafb
marios <mgeorgak859@gmail.com>
2022-07-11 17:34:32 +0300

    added newPostAction

---
+++ linkedin/src/components/NewPost.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+
+
+
+
+function NewPost () {
+    return(
+        <div>NewPost</div>
+    )
+}
+
+export default NewPost
--- linkedin/src/redux/actions/index.js
+++ linkedin/src/redux/actions/index.js
@@ -25,3 +25,35 @@ export const getPostsAction = () => {
         }
     }
 }
+
+export const newPostAction = () => {
+    return async (dispatch, getState) => {
+        try {
+            let text ="hey i am a post"
+            let response = await fetch (
+                "https://striveschool-api.herokuapp.com/api/posts/", {
+          method: 'POST',
+          headers: {
+            'Accept' : 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+            Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmNiZWNlY2U2YzAzMDAwMTU5MTgxNDMiLCJpYXQiOjE2NTc1MzE2MjgsImV4cCI6MTY1ODc0MTIyOH0.Ueo_M62QO05ffN1aYIPJjOyI14bH3uldPPo-OlagobM",
+          },
+          body: JSON.stringify(text)
+        }
+            );
+            if (response.ok) {
+                let newPost = await response.json();
+                console.log(newPost)
+                dispatch({
+                  type: 'NEW_POST',
+                  payload: newPost,
+                })
+             
+  
+              }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
