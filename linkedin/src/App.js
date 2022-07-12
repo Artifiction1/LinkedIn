34c9d9ec540f81b9fc65eabdff208baca2514da2
marios <mgeorgak859@gmail.com>
2022-07-12 16:10:32 +0300

    changes

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,19 +1,16 @@
 
 import './App.css';
-import GetPosts from './components/GetPosts';
-import NewPost from './components/NewPost';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Details from './components/Details';
+import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <NavBar />
       <Routes>
-      <Route path="/" element={<GetPosts />} />
-      <Route path="/posts" element={<NewPost />} />
-      <Route path="/details/:postId" element={<Details />} />
+      
       </Routes>
       </BrowserRouter>
     </div>
