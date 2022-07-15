01cacb280731fb50c04c215b569b6d9a88d42e54
Artifiction1 <angelus1@inbox.lv>
2022-07-15 14:39:09 +0300

    Update Experiences.jsx

--- linkedin/src/components/Experiences.jsx
+++ linkedin/src/components/Experiences.jsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToExp } from "../redux/actions/actions";
 import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 import ExpComponent from "./ExpComponent";
 
 const mapStateToProps = (state) => {
@@ -19,11 +20,16 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
   addToExperience: (Exp, target) => dispatch(addToExp(Exp, target)),
 });
-const Experiences = ({ Experience, addToExperience }) => {
+const Experiences = ({ Experience, addToExperience, id }) => {
+  const params = useParams();
+  let userId = params.userId;
+  if(userId === undefined){
+    userId = "62cbecece6c0300015918143"
+  }
   const [show, setShow] = useState(false);
   let expMe =
     "https://striveschool-api.herokuapp.com/api/profile/62cbecece6c0300015918143/experiences";
-  const handleClose = () => setShow(false);
+  const handleClose = () => setShow(false);//62cbecece6c0300015918143
   const handleShow = () => setShow(true);
   const [toggle, setToggle] = useState(false);
   const [changed, setChanged] = useState("");
@@ -61,7 +67,7 @@ const Experiences = ({ Experience, addToExperience }) => {
     } else {
       try {
         const response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/profile/62cbecece6c0300015918143/experiences",
+          "https://striveschool-api.herokuapp.com/api/profile/"+userId+"/experiences",
           {
             method: method,
             headers: {
@@ -89,17 +95,16 @@ const Experiences = ({ Experience, addToExperience }) => {
       return `Present`;
     }
   };
-
   useEffect(() => {
     FetchExperiences("GET");
-  }, []);
+  }, [userId]);
 
   if (NewExperiences.length > 0) {
     return (
       <div className="experiences">
         <div className="div-edu">
           <h3>Experience</h3>
-          <Plus
+          {userId === '62cbecece6c0300015918143' && (<Plus
             onClick={handleShow}
             size="2.5rem"
             style={{
@@ -107,7 +112,7 @@ const Experiences = ({ Experience, addToExperience }) => {
               marginLeft: "46rem",
               marginTop: "-5rem",
             }}
-          />
+          />)}
           <Modal show={show} onHide={handleClose} animation={false}>
             <Modal.Header>
               <Modal.Title>Add Experience</Modal.Title>
@@ -190,7 +195,7 @@ const Experiences = ({ Experience, addToExperience }) => {
             </Modal.Footer>
           </Modal>
 
-          <Pencil
+          {userId === '62cbecece6c0300015918143' &&(<Pencil
             size="1.2rem"
             style={{
               cursor: "pointer",
@@ -201,7 +206,7 @@ const Experiences = ({ Experience, addToExperience }) => {
               setToggle(!toggle);
               FetchExperiences("GET");
             }}
-          />
+          />)}
         </div>
         {NewExperiences.map((exp) => (
           <div className="d-flex justify-content-between" key={exp._id}>
@@ -209,29 +214,39 @@ const Experiences = ({ Experience, addToExperience }) => {
               <div className="d-flex">
                 {toggle === false && (
                   <>
-                    <img
-                      src="https://media-exp2.licdn.com/dms/image/C4D0BAQEFWO_s8a0FHQ/company-logo_200_200/0/1647618816994?e=1665619200&amp;v=beta&amp;t=hzqVGRvol3rh_0b7B5xMv2kmIgcVfGUICHu6g2OYAus"
-                      loading="lazy"
-                      width="68px"
-                      height="68px"
-                      alt="EPICODE logo"
-                    />
 
-                    <div className="m-1 ml-4 d-flex flex-column text-left">
-                      <h6>{exp.role}</h6>
-                      <span id="eduFontSize">{exp.company}</span>
-                      <span id="eduFontSize" className="text-muted">
-                        {exp.startDate.substr(0, 10)} -{" "}
-                        {exp.endDate !== undefined && (
-                          <span>{exp.endDate.substr(0, 10)}</span>
-                        )}{" "}
-                        {exp.endDate === undefined && <span>now</span>}
-                      </span>
-                      <span id="eduFontSize">{exp.area}</span>
-                    </div>
-                    <hr style={{ color: "blue" }} />
-                  </>
+                  {exp.image === undefined &&(
+                <img
+                  src="https://media-exp2.licdn.com/dms/image/C4D0BAQEFWO_s8a0FHQ/company-logo_200_200/0/1647618816994?e=1665619200&amp;v=beta&amp;t=hzqVGRvol3rh_0b7B5xMv2kmIgcVfGUICHu6g2OYAus"
+                  loading="lazy"
+                  width="68px"
+                  height="68px"
+                  alt="EPICODE logo"
+                />)}
+                {exp.image !== undefined && (
+                  <img
+                  src={exp.image}
+                  loading="lazy"
+                  width="68px"
+                  height="68px"
+                  alt="EPICODE logo"
+                />
                 )}
+                
+                  <div className="m-1 ml-4 d-flex flex-column text-left">
+                    <h6>{exp.role}</h6>
+                    <span id="eduFontSize">{exp.company}</span>
+                    <span id="eduFontSize" className="text-muted">
+                      {exp.startDate.substr(0, 10)} -{" "}
+                      {exp.endDate !== undefined && (
+                        <span>{exp.endDate.substr(0, 10)}</span>
+                      )}{" "}
+                      {exp.endDate === undefined && <span>now</span>}
+                    </span>
+                    <span id="eduFontSize">{exp.area}</span>
+                  </div>
+                </>)}
+
                 {toggle === true && <ExpComponent experience={exp} />}
               </div>
             </div>
